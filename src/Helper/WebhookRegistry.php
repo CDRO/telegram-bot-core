2b060a78755064c7ac2f7edd1053adefff4a1e5b
Tizian Schmidlin <st@cabag.ch>
2020-06-04 00:29:41 +0200

    [FEATURE] Add Webhook registry helper

---
+++ src/Helper/WebhookRegistry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cdro\TelegramBotCore\Helper;
+
+use \Cdro\TelegramBotCore\Registry\AbstractRegistry;
+
+class WebhookRegistry
+{
+    /**
+     * The registry to save to
+     *
+     * @var AbstractRegistry
+     */
+    private $registry;
+
+    public function __construct(AbstractRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+
+    public function handle()
+    {
+        $input = file_get_contents('php://input');
+
+        $this->registry->handle($input->message);
+    }
+}
