8ee537686e17aaf8aafb87536c5dcd7520196180
Tizian Schmidlin <st@cabag.ch>
2020-06-04 13:26:32 +0200

    [BUGFIX] Fix method declaration

--- src/Security/LayerInterface.php
+++ src/Security/LayerInterface.php
@@ -6,7 +6,6 @@ interface LayerInterface {
 
     /**
      * Make the security check and execute a callback
-     * @param mixed $callback
      */
-    public function check($options = null);
+    public function check();
 }
--- src/Security/SimpleLayer.php
+++ src/Security/SimpleLayer.php
@@ -6,9 +6,12 @@ class SimpleLayer implements LayerInterface {
 
     /**
      * Make the security check and execute a callback
-     * @param mixed $callback
+     * @param callable|null $options
      */
-    public function check(callable $options) {
-        $options();
+    public function check(callable $options = null)
+    {
+        if(!is_null($options)) {
+            $options();
+        }
     }
 }
